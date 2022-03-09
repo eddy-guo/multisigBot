ab393295e79623af7e7223a083617003838b5f58
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-09 17:26:44 -0500

    env and bot setup

---
+++ ms.py
@@ -0,0 +1,12 @@
+import os, discord, pdb
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
