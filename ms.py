9a9afdb3f2a426278158fbbb9ae6b5ae41ee0b86
Eddy Guo <eddyyanruguo@gmail.com>
2022-03-22 07:23:19 -0400

    dict logic to track user messages; cleaner commands

--- ms.py
+++ ms.py
@@ -17,6 +17,10 @@ client = discord.Client()
 async def on_ready():
     global guild
     guild = discord.utils.get(bot.guilds, name=GUILD)
+    global user_messages
+    user_messages = {}
+    for member in guild.members:
+        user_messages[member.name] = "filler"
     print(f"{bot.user} is now connected to {guild.name}.")
 
 @bot.event
@@ -26,6 +30,12 @@ async def on_message(message):
     ctx = await bot.get_context(message)
     if message.channel.id == 955726679207198741:
         if message.content == "!announce":
+            await ctx.send(f"What would you like to announce, <@" + str(message.author.id) + '>?')
+    
+    for user in user_messages:
+        if user_messages[user] == "!announce" and user == message.author.name:
             await announce.announcement(ctx, message.content)
+    user_messages[message.author.name] = message.content
+    print(user_messages)
 
 bot.run(TOKEN)
