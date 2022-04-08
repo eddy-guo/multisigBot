6f449ba2f3f71c72bd381686b5c6811bc2691a41
Eddy Guo <eddyyanruguo@gmail.com>
2022-04-08 11:11:33 -0400

    send msg to announcements if successful

--- ms.py
+++ ms.py
@@ -2,6 +2,7 @@ import os, discord, pdb
 import announce
 from dotenv import load_dotenv
 from discord.ext import commands
+from announce import *
 
 load_dotenv()
 
@@ -42,6 +43,12 @@ async def on_message(message):
 
     if user_messages[message.author.name] == "!announce":
         await announce.announcement(ctx, message.content)
+        channel = bot.get_channel(955726679207198741)
+        list = await channel.history(limit=1).flatten()
+        if list[0].content == "**Announcement has been approved by moderators and will be sent to the announcements text channel.**":
+            channel = bot.get_channel(961988047409414244)
+            await channel.send(message.content)
+            await ctx.send("**Announcement was successfully sent!**")
 
     if role in message.author.roles and message.channel.id == 955726679207198741:
         user_messages[message.author.name] = message.content
