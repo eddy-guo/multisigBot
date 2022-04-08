7ec8e4b7d342d62288f10d51ed6240bb1997beb2
Eddy Guo <eddyyanruguo@gmail.com>
2022-04-08 10:28:16 -0400

    edit response text and checkreaction messages

--- announce.py
+++ announce.py
@@ -14,11 +14,12 @@ async def announcement(ctx, message):
                       f'and only when the previous announcement is finished its evaluation.')
     else:
         global response
-        response = await ctx.send(f"You just sent {message}!")
-
         good = emoji.emojize(':check_mark_button:')
         bad = emoji.emojize(':cross_mark:')
-
+        response = await ctx.send('__Please evaluate the following message and decide whether ' 
+                                 f'it is appropriate as a server-wide announcement. '
+                                 f'Vote by reacting on {good} and {bad} for **in favor** and **against** respectively.__'
+                                 f'\n\n{message}')
         await response.add_reaction(good)
         await response.add_reaction(bad)
 
@@ -27,6 +28,6 @@ async def checkreaction(ctx):
     await asyncio.sleep(5)
     updated = await response.channel.fetch_message(response.id)
     if updated.reactions[0].count > updated.reactions[1].count:
-        await ctx.send("Announcement is good to send! (evaluation complete)")
+        await ctx.send("**Announcement has been approved by moderators and will be sent to the announcements text channel.**")
     else:
-        await ctx.send('Announcement is not approved. (evaluation complete)')
+        await ctx.send('**Announcement has been rejected by moderators and will not be sent.**')
