7f9ee0a7923b8cdc9974d24d430174ed9030be1d
Eddy Guo <eddyyanruguo@gmail.com>
2022-04-08 09:35:27 -0400

    edit error message

--- announce.py
+++ announce.py
@@ -1,7 +1,6 @@
 import os, discord, pdb, asyncio, emoji
 from dotenv import load_dotenv
 from discord.ext import commands
-from asyncio import sleep
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,7 +10,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def announcement(ctx, message):
     if message == "!announce":
-        await ctx.send(f"You can only announce one message at a time. Please try again.")
+        await ctx.send('You can only announce one message at a time, and only when the previous announcement is finished its evaluation.')
     else:
         global response
         response = await ctx.send(f"You just sent {message}!")
@@ -21,7 +20,6 @@ async def announcement(ctx, message):
 
         await response.add_reaction(good)
         await response.add_reaction(bad)
-        response = await response.channel.fetch_message(response.id)
 
 @bot.command()
 async def checkreaction(ctx):
@@ -31,4 +29,3 @@ async def checkreaction(ctx):
         await ctx.send("Announcement is good to send!")
     else:
         await ctx.send('Announcement is not approved.')
-    
